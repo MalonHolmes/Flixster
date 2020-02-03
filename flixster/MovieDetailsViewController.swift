a07ba9e08bd6b28860836e52773b135b302bf7ce
Malon Holmes <itsmalonholmes@gmail.com>
2020-02-03 02:19:51 -0500

    Incomplete submission of unit 2 assisgnment

--- flixster/MovieDetailsViewController.swift
+++ flixster/MovieDetailsViewController.swift
@@ -26,13 +26,21 @@ class MovieDetailsViewController: UIViewController {
         print(movie["title"])
         
         titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
         synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
         
         let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
         
         posterView.af_setImage(withURL: posterUrl!)
+    
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        backdropView.af_setImage(withURL: backdropUrl!)
+        //backdropView.sizeToFit()
     }
     
 
---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by JAMIE HOLMES on 2/3/20.
+//  Copyright © 2020 Malon Holmes. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
